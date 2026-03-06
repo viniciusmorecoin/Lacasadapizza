5bdb666d73ca5138c1c15a41e4051b11c376dcdb
viniciusmorecoin <38147149+viniciusmorecoin@users.noreply.github.com>
2026-03-06 01:30:17 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,35 +2,14 @@ const CACHE_NAME = 'lacasa-cache-v1';
 const assets = [
   './',
   './index.html',
-  './manifest.json',
   './logo.png',
-  './icon-192.png',
-  './icon-512.png'
+  './icon-192.png'
 ];
 
-// Instalação: Salva arquivos essenciais no cache
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// Ativação: Limpa caches antigos
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
-});
-
-// Busca: Serve do cache se estiver offline
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
